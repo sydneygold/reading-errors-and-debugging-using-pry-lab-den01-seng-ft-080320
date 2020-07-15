6cf12e451f8178a37869801ecb80fd62379548ff
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-15 21:17:45 +0000

    Automatically backed up by Learn

--- spec/false_equivalency_spec.rb
+++ spec/false_equivalency_spec.rb
@@ -1,5 +1,4 @@
-require 'pry'
-require_relative './fix_using_tests/false_equivalency'
+require './fix_using_tests/false_equivalency'
 
 describe "false_equivalency" do
   it "`selection` returns the correct string based on user input" do
