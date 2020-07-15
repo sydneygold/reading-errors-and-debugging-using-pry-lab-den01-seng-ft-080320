8b42009c11f4e1a521d42f2d08ed6deec752851d
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-15 22:12:57 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -5,7 +5,9 @@ def start_game(player1, player2)
    "Hello #{player1} & #{player2}"
 end
 
-def play_game(player2)
+player1 = "Jay-Z"
+player2 = "Beyonce"
+def play_game(player1, player2)
   puts "#{player1} is better than #{player2}"
 
 end
