0faea9f90cff3c63ba03bce4f6dc5530a784ddd5
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-15 23:03:27 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -4,6 +4,9 @@ def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
 
+def star_date(date)
+  star_date = generate_star_date
+end
 
 def state_log(star_date)
   "Captain's Log, star date #{star_date}."
@@ -11,5 +14,5 @@ end
 
 def engage
    state_log(star_date)
-  star_date = generate_star_date
+  date = generate_star_date
 end
