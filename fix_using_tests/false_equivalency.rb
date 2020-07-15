1adb3f40a5e8d8bc10e52236ea0bec41fbc14294
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-15 21:49:09 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -14,9 +14,10 @@ end
 def selection(num)
   if num == 1
     puts "YUM YUM MUNCH MUNCH MUNCH"
-    binding.pry
+    
   else num == 2
     puts "HAM HAM HAM IN MY TUMMY"
+    binding.pry
 end
 end
 
