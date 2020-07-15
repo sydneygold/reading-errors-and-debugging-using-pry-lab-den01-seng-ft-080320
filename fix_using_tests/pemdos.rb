9856568699f62e7689d4c20986a69af6570df269
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-15 22:36:47 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,3 +1,12 @@
+require 'pry'
+def snake_it_up(string)
+  if string[0] == "s"
+"s".to_i * 10 + string.to_i
+  else
+    string
+  end
+end
+binding.pry
 
 def snake_it_up(string)
   if string[0] == "s"
