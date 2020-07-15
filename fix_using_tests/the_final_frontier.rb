0b438e912428fc25ab0c489330b797073cfcca92
Sydney Goldberg <sydney.goldberg@outlook.com>
2020-07-15 23:12:47 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -4,8 +4,8 @@ require 'pry'
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
-def star_date(generate_star_date)
-    star_date = generate_star_date
+def star_date(date,generate_star_date)
+    date = generate_star_date
 end
 
 def state_log(star_date)
